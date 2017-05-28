ebe59e8936c75a8fe1c2a970cce225b616da18bb
Paul Cuzner <pcuzner@redhat.com>
2017-05-29 11:24:50 +1200

    utils - added additional generic functions used by the osd class
    The osd class needs to read specific files for disk configuration and
    performance stats. utils now includes simple fread and freadlines
    functions to simplify the main osd class

--- collectors/utils.py
+++ collectors/utils.py
@@ -3,8 +3,10 @@
 
 import socket
 
+
 def get_hostname():
     return socket.gethostname().split('.')[0]
+
 
 def add_dicts(dict1, dict2):
     """
@@ -33,7 +35,58 @@ def merge_dicts(dict1, dict2):
 
 
 def flatten_dict(data, separator='.', prefix=''):
+    """
+    flatten a dict, so it is just simple key/value pairs
+    :param data: (dict)
+    :param separator: (str) char to use when combining keys
+    :param prefix: key prefix
+    :return:
+    """
     return {prefix + separator + k if prefix else k: v
             for kk, vv in data.items()
             for k, v in flatten_dict(vv, separator, kk).items()
             } if isinstance(data, dict) else {prefix: data}
+
+
+def todict(obj):
+    """
+    convert an object to a dict representation
+    :param obj: (object) object to examine, to extract variables/values from
+    :return: (dict) representation of the given object
+    """
+    data = {}
+    for key, value in obj.__dict__.iteritems():
+
+        if key.startswith('_'):
+            continue
+
+        try:
+            data[key] = todict(value)
+        except AttributeError:
+            data[key] = value
+    
+    return data
+
+
+def fread(file_name=None):
+    """
+    Simple read function for files of a single value
+    :param file_name: (str) file name to read
+    :return: (str) contents of the file
+    """
+
+    with open(file_name, 'r') as f:
+        setting = f.read().rstrip()
+    return setting 
+
+
+def freadlines(file_name=None):
+    """
+    simple readlines function to return all records of a given file
+    :param file_name: (str) file name to read
+    :return: (list) contents of the file
+    """
+
+    with open(file_name, 'r') as f:
+        data = f.readlines()
+    return data
